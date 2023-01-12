ce5a9ee9d4568dd572d273825ab71574023c58f4
Atef-Abrar-Bhuyian <abrarbhuyian8@gmail.com>
2023-01-12 21:44:58 +0600

    Add Repo

--- index.html/dbconnect.php
+++ index.html/dbconnect.php
@@ -1,5 +1,5 @@
 <?php
-    $con=mysqli_connect("Localhost", "root", "", "medigo");
+    $con=mysqli_connect("brkvxgksob4hbhu7ylhb-mysql.services.clever-cloud.com", "uc3o89ljsybscr6f", "zOzxjgbPTCmb3plpI3oH", "brkvxgksob4hbhu7ylhb");
     if(mysqli_connect_errno()){
     echo "Connection Fail".mysqli_connect_error();
     }
